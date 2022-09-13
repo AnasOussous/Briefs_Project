46338c55c623d5dba7c576d1ebe60c199030093e
idmed22 <idlmhor.med@gmail.com>
2022-09-13 09:13:38 +0100

    add service & controller

---
+++ Brief 16 - Souss_Health/src/main/java/com/Souss_Health_Brief16/controller/UserController.java
@@ -0,0 +1,93 @@
+package com.Souss_Health_Brief16.controller;
+
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import com.Souss_Health_Brief16.model.User;
+import com.Souss_Health_Brief16.repository.UserRepository;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+	
+	@Autowired
+	  private UserRepository userRepository;
+	
+	
+	@GetMapping("/users")
+	  public List<User> getAllUsers() {
+	    return userRepository.findAll();
+	  }
+	
+	
+	@GetMapping("/users/{id}")
+	  public ResponseEntity<User> getUsersById(@PathVariable(value = "id") Long userId)
+	      throws ResourceNotFoundException {
+	    User user =
+	        userRepository
+	            .findById(userId)
+	            .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + userId));
+	    return ResponseEntity.ok().body(user);
+	  }
+	
+	
+	@PostMapping("/users")
+	  public User createUser(@Valid @RequestBody User user) {
+	    return userRepository.save(user);
+	  }
+	
+	
+	 @PutMapping("/users/{id}")
+	  public ResponseEntity<User> updateUser(
+	      @PathVariable(value = "id") Long userId, @Valid @RequestBody User userDetails)
+	      throws ResourceNotFoundException {
+
+	    User user =
+	        userRepository
+	            .findById(userId)
+	            .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + userId));
+	    
+       user.setNom(userDetails.getNom());
+       user.setPrenom(userDetails.getPrenom());
+       user.setPassword(userDetails.getPassword());
+       user.setEmail(userDetails.getEmail());
+       user.setTelephone(userDetails.getTelephone());
+       user.setUsername(userDetails.getUsername());
+	    
+	   
+	    final User updatedUser = userRepository.save(user);
+	    return ResponseEntity.ok(updatedUser);
+	  }
+	 
+	 @DeleteMapping("/user/{id}")
+	  public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long userId) throws Exception {
+	    User user =
+	        userRepository
+	            .findById(userId)
+	            .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + userId));
+
+	    userRepository.delete(user);
+	    Map<String, Boolean> response = new HashMap<>();
+	    response.put("deleted", Boolean.TRUE);
+	    return response;
+	  }
+
+}
--- Brief 16 - Souss_Health/src/main/java/com/Souss_Health_Brief16/model/Admin.java
+++ Brief 16 - Souss_Health/src/main/java/com/Souss_Health_Brief16/model/Admin.java
@@ -11,5 +11,7 @@ public class Admin extends User {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
+    
+	
+	
 }
